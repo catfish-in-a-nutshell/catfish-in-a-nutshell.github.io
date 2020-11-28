7b87c67e8044ef0b71005daeb34c52d0658262ae
catfish-in-a-nutshell <while1incrp@hotmail.com>
2020-11-28 22:19:12 +0800

    add translation for index & rws

--- rws/index.js
+++ rws/index.js
@@ -14,14 +14,14 @@
 		var miracleRNG = null;
 
 		var breathRNG = null;
-        var breathNames = ['Red', 'Green', 'Blue', 'White', 'Black'];
+        var breathNames = ['红', '绿', '蓝', '白', '黑'];
         var breathTier = 1;
         var maelstromRNG = null;
         var maelstromTargets = 3;
-        var maelstromEffects = ['Mana Produced', 'Trophies Unlocked', 'Faction Coins found', 'Amount of Assistants'];
+        var maelstromEffects = ['魔产量', '已获得成就数', '种族币获得', '助手数量'];
 
         var limitedWishRNG = null;
-        var limitedWishEffects = ["Increase the production of all buildings", "Increase Assistants", "Increase Maximum Mana", "Increase Trophy Count and Offline Bonus", "Increase Faction Coin find chance", "Increase Mana Regeneration", "All Spell Durations count more"];
+        var limitedWishEffects = ["提升所有建筑产能", "提升助手数量", "提升魔上限", "提升已获得成就数结算值和离线产能加成", "提升种族币获得率", "提升回魔", "延长所有法术持续时间"];
         var limitedWishEligibleEffects = [];
         var limitedWishActivityTime = 0;
         var limitedWishCastCount = 0;
@@ -64,7 +64,7 @@
                     buildingsAvailable.push(i);
                 }
             }
-			$('#buildings').html('<b>Buildings owned</b> <small><i>(Click a building to toggle its highlighting)</i></small><br>');
+			$('#buildings').html('<b>拥有建筑</b> <small><i>(点击一个建筑可以高亮)</i></small><br>');
 			for (var tier in buildingsOwned) {
 				var hit = buildingNames[buildingsOwned[tier]];
 				var span = $('<span />').html(hit).addClass('tier' + tier).data('tier', tier);
@@ -88,32 +88,32 @@
 
 		// Check if the save actually has Lightning Strikes to forecast
 			if (!util.save.hasSpell(save,"Lightning Strike")) {
-			 	lightningMessage = 'You don\'t have Lightning Strike.';
-			 	lightningForecast = 'No Lightning.';
+			 	lightningMessage = '你未拥有雷击术。';
+			 	lightningForecast = '无雷击。';
 			} else if (save.alignment != 3 && !util.save.upgrade_owned(save, 688)) {
-				lightningMessage = 'You are not Neutral aligned.';
-				lightningForecast = 'No Lightning.';
+				lightningMessage = '你未属于中立阵营。';
+				lightningForecast = '无雷击。';
             		} else if (buildingsOwned.length == 0) {
-				lightningMessage = 'You have no Buildings.';
-				lightningForecast = 'No Lightning.';
+				lightningMessage = '你没有建筑。';
+				lightningForecast = '无雷击。';
 			} else if (buildingsOwned.length == 1) {
-				lightningMessage = 'You only have ' + buildingNames[buildingsOwned[0]] + '.';
-				lightningForecast = 'The only Building you have, as often as you want.';
+				lightningMessage = '你只有 ' + buildingNames[buildingsOwned[0]] + '.';
+				lightningForecast = '你有的唯一一个建筑，想击中多久就多久。';
 			}
 
 			// Early exit
 			if (lightningMessage != '' || lightningForecast != '') {
-				$('#lightningMessage').html('<b>Lightning Strike</b><br>').append(lightningMessage);
-				$('#lightningForecast').html('<b>Forecast</b><br>').append(lightningForecast);
+				$('#lightningMessage').html('<b>雷击</b><br>').append(lightningMessage);
+				$('#lightningForecast').html('<b>预报</b><br>').append(lightningForecast);
 				return;
 			}
 
 			// Create the RNG and get the initial forecast
 			lightningRNG = new PM_PRNG(save.spells[13].s);
 			lightningRNG.hasLightningRod = util.save.upgrade_owned(save, 143018);
-			$('#lightningMessage').html('<b>Lightning Strike</b><br>Your RNG state is: ' + lightningRNG.state + '.');
-			$('#lightningForecast').html('<b>Forecast</b> <small><i>(Click a building to toggle its highlighting)</i></small><br><ol></ol>')
-				.append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastLightningMore));
+			$('#lightningMessage').html('<b>雷击</b><br>你的随机数生成器状态: ' + lightningRNG.state + '.');
+			$('#lightningForecast').html('<b>预报</b> <small><i>(点击一个建筑可以高亮)</i></small><br><ol></ol>')
+				.append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastLightningMore));
 
 			forecastLightningMore();
 		};
@@ -128,28 +128,28 @@
 
 			// Check if the save actually has Miracles to forecast
 			if (!miracle || !miracle.u1) {
-				miracleMessage = 'You can\'t get any Miracles yet.';
-				miracleForecast = 'No Miracles.';
+				miracleMessage = '你还无法获得任何奇迹。';
+				miracleForecast = '无奇迹。';
 			} else if (buildingsOwned.length == 0) {
-				miracleMessage = 'You have no Buildings.';
-				miracleForecast = 'No Miracles.';
+				miracleMessage = '你没有建筑';
+				miracleForecast = '无奇迹。';
 			} else if (buildingsOwned.length == 1) {
-				miracleMessage = 'You only have ' + buildingNames[buildingsOwned[0]] + '.';
-				miracleForecast = 'The only Building you have, as long as you want.';
+				miracleMessage = '你只有 ' + buildingNames[buildingsOwned[0]] + '.';
+				miracleForecast = '你有的唯一一个建筑，想持续多久就多久。';
 			}
 
 			// Early exit
 			if (miracleMessage != '' || miracleForecast != '') {
-				$('#miracleMessage').html('<b>Miracle</b><br>').append(miracleMessage);
-				$('#miracleForecast').html('<b>Forecast</b><br>').append(miracleForecast);
+				$('#miracleMessage').html('<b>奇迹</b><br>').append(miracleMessage);
+				$('#miracleForecast').html('<b>预报</b><br>').append(miracleForecast);
 				return;
 			}
 
 			// Create the RNG and get the initial forecast
 			miracleRNG = new PM_PRNG(miracle.s);
-			$('#miracleMessage').html('<b>Miracle</b><br>Your RNG state is: ' + miracleRNG.state + '.');
-			$('#miracleForecast').html('<b>Forecast</b> <small><i>(Click a building to toggle its highlighting)</i></small><br><ol></ol>')
-				.append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastMiracleMore));
+			$('#miracleMessage').html('<b>奇迹</b><br>你的随机数生成器状态: ' + miracleRNG.state + '.');
+			$('#miracleForecast').html('<b>预报</b> <small><i>(点击一个建筑可以高亮)</i></small><br><ol></ol>')
+				.append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastMiracleMore));
 
 			forecastMiracleMore();
 		};
@@ -161,14 +161,14 @@
 
 			// Check if the save actually has Dragons Breath to forecast
 			if (!util.save.hasSpell(save,"Dragon's Breath")) {
-				breathMessage = 'You don\'t have Dragon\'s Breath.';
-				breathForecast = 'No Dragon\'s Breath.';
+				breathMessage = '你未拥有龙息术。';
+				breathForecast = '无龙息。';
 			}
 
 			// Early exit
 			if (breathMessage != '' || breathForecast != '') {
-				$('#breathMessage').html('<b>Dragon\'s Breath</b><br>').append(breathMessage);
-				$('#breathForecast').html('<b>Forecast</b><br>').append(breathForecast);
+				$('#breathMessage').html('<b>龙息</b><br>').append(breathMessage);
+				$('#breathForecast').html('<b>预报</b><br>').append(breathForecast);
 				return;
 			}
 
@@ -181,9 +181,9 @@
                 breathTier *= 2;
             }
 
-			$('#breathMessage').html('<b>Dragon\'s Breath</b><br>Your RNG state is: ' + breathRNG.state + '.');
-			$('#breathForecast').html('<b>Forecast</b><br><ol></ol>')
-				.append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastBreathMore));
+			$('#breathMessage').html('<b>龙息</b><br>你的随机数生成器状态: ' + breathRNG.state + '.');
+			$('#breathForecast').html('<b>预报</b><br><ol></ol>')
+				.append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastBreathMore));
 
 			forecastBreathMore();
 		};
@@ -276,23 +276,23 @@
 
             // Check if the save actually has Maelstrom to forecast
             if (!util.save.hasSpell(save,"Maelstrom")) {
-                maelstromMessage = 'You don\'t have Maelstrom.';
-                maelstromForecast = 'No Chaos that is trying to pull you in.';
+                maelstromMessage = '你未拥有大漩涡.';
+                maelstromForecast = '还未有混沌试图将你卷入。';
             }
 
             // Early exit
             if (maelstromMessage != '' || maelstromForecast != '') {
-                $('#maelstromMessage').html('<b>Maelstrom</b><br>').append(maelstromMessage);
-                $('#maelstromForecast').html('<b>Forecast</b><br>').append(maelstromForecast);
+                $('#maelstromMessage').html('<b>大漩涡</b><br>').append(maelstromMessage);
+                $('#maelstromForecast').html('<b>预报</b><br>').append(maelstromForecast);
                     return;
             }
 
             // Create the RNG and get the initial forecast
             maelstromRNG = new PM_PRNG(save.spells[27].s);
 
-            $('#maelstromMessage').html('<b>Maelstrom</b><br>Your RNG state is: ' + maelstromRNG.state + '.');
-            $('#maelstromForecast').html('<b>Forecast</b><br><ol></ol>')
-                .append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastMaelstromMore));
+            $('#maelstromMessage').html('<b>大漩涡</b><br>你的随机数生成器状态: ' + maelstromRNG.state + '.');
+            $('#maelstromForecast').html('<b>预报</b><br><ol></ol>')
+                .append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastMaelstromMore));
 
             forecastMaelstromMore();
 		};
@@ -361,14 +361,14 @@
 
             // Check if the save actually has Limited Wish to forecast
             if (!util.save.hasSpell(save,"Limited Wish")) {
-                limitedWishMessage = 'You don\'t have Limited Wish.';
-                limitedWishForecast = 'The Genie is in an another lamp.';
+                limitedWishMessage = '你未拥有有限祈愿。';
+                limitedWishForecast = '灯神在另一盏灯里。';
             }
 
             // Early exit
             if (limitedWishMessage != '' || limitedWishForecast != '') {
-                $('#limitedWishMessage').html('<b>Limited Wish</b><br>').append(limitedWishMessage);
-                $('#limitedWishForecast').html('<b>Forecast</b><br>').append(limitedWishForecast);
+                $('#limitedWishMessage').html('<b>有限祈愿</b><br>').append(limitedWishMessage);
+                $('#limitedWishForecast').html('<b>预报</b><br>').append(limitedWishForecast);
 		$('#limitedWishForecastOptions').hide();
 		return;
             }
@@ -423,9 +423,9 @@
                 limitedWishEligibleEffects.push(limitedWishEffects[4]);
             }
 
-            $('#limitedWishMessage').html('<b>Limited Wish</b><br>Your RNG state is: ' + limitedWishRNG.state + '.');
-            $('#limitedWishForecast, #limitedWishForecastAlt').html('<b>Forecast</b><br><ol></ol>')
-                .append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastLimitedWishMore));
+            $('#limitedWishMessage').html('<b>有限祈愿</b><br>你的随机数生成器状态: ' + limitedWishRNG.state + '.');
+            $('#limitedWishForecast, #limitedWishForecastAlt').html('<b>预报</b><br><ol></ol>')
+                .append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastLimitedWishMore));
 
             forecastLimitedWishMore();
 	    limitedWishShowForecast();
@@ -443,8 +443,8 @@
 
 				//Due to Djinn perk 3 we can no longer accurately calculate limitedWish
 				//create two results for both value and percentage
-				var textResult = limitedWishEligibleEffects[typeHit] + ' with random value of ' + strengthHit.toLocaleString() + " (out of maximum of " + limitedWishCastCount.toLocaleString() + ").";
-				var textResult2 = limitedWishEligibleEffects[typeHit] + ' with ' + ((strengthHit**0.45 / limitedWishCastCount**0.45) * 100).toPrecision(4) + '% strength';
+				var textResult = limitedWishEligibleEffects[typeHit] + ' 随机值为 ' + strengthHit.toLocaleString() + " (最大值 " + limitedWishCastCount.toLocaleString() + ").";
+				var textResult2 = limitedWishEligibleEffects[typeHit] + '，幅度为' + ((strengthHit**0.45 / limitedWishCastCount**0.45) * 100).toPrecision(4) + '%';
 				var li = $('<li />').html(textResult);
 				var li2 = $('<li />').html(textResult2);
 				$('#limitedWishForecast > ol').append(li);
@@ -481,15 +481,15 @@
             var catalystForecast = '';
 
             // Check if the save actually has Catalyst to forecast
-	    if (!util.save.hasSpell(save,"Catalyst")) {
-                catalystMessage = 'You don\'t have Catalyst.';
-                catalystForecast = 'Who knew chaotic blood was so magical?';
+	    if (!util.save.hasSpell(save,"催化剂")) {
+                catalystMessage = '你未拥有催化剂。';
+                catalystForecast = '谁知道混乱之血有这种魔力呢？';
             }
 
             // Early exit
             if (catalystMessage != '' || catalystForecast != '') {
-                $('#catalystMessage').html('<b>Catalyst</b><br>').append(catalystMessage);
-                $('#catalystForecast').html('<b>Forecast</b><br>').append(catalystForecast);
+                $('#catalystMessage').html('<b>催化剂</b><br>').append(catalystMessage);
+                $('#catalystForecast').html('<b>预报</b><br>').append(catalystForecast);
                     return;
             }
 
@@ -502,11 +502,11 @@
             catalystEligibleEffects = catalystEffects.filter(spell => !util.save.hasSpell(save,spell));
 			//ugh2
 	    var spellIDs = [null,null,'Holy Light','Blood Frenzy','Gem Grinder',null,null,null,'Fairy Chanting','Moon Blessing', 'God\'s Hand', 'Goblin\'s Greed', 'Night Time', 'Hellfire Blast'];
-            $('#catalystMessage').html('<b>Catalyst</b><br>Your RNG state is: ' + catalystRNG.state + '.');
-	    if(save.catalystTargets.length > 0) $('#catalystCurrent').html('Your current spell is: ' + spellIDs[save.catalystTargets[0]['targetspell']]);
-	    if(save.catalystTargets.length > 1) $('#catalystCurrent').append('<br>' + 'Your second current spell is: ' + spellIDs[save.catalystTargets[1]['targetspell']]);
-            $('#catalystForecast').html('<b>Forecast</b><br><ol></ol>')
-                .append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastCatalystMore));
+            $('#catalystMessage').html('<b>催化剂</b><br>你的随机数生成器状态: ' + catalystRNG.state + '.');
+	    if(save.catalystTargets.length > 0) $('#catalystCurrent').html('你当前的法术是: ' + spellIDs[save.catalystTargets[0]['targetspell']]);
+	    if(save.catalystTargets.length > 1) $('#catalystCurrent').append('<br>' + '你当前的第二法术是: ' + spellIDs[save.catalystTargets[1]['targetspell']]);
+            $('#catalystForecast').html('<b>预报</b><br><ol></ol>')
+                .append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastCatalystMore));
 
             forecastCatalystMore();
 		};
@@ -529,14 +529,14 @@
 
 	var forecastDJC4 = function(save) {
 		if(!(util.save.challenge_active(save,992))) {
-			$('#DJC4Message').html('<b>Worldly Desires</b><br>There is no desire here...');
-			$('#DJC4Forecast').html('<b>Forecast</b><br>No Worldly Desires.');
+			$('#DJC4Message').html('<b>世界之愿</b><br>这里没有愿望……');
+			$('#DJC4Forecast').html('<b>预报</b><br>没有世界之愿。');
 			return;
 		}
 		DJC4RNG = new PM_PRNG(save.upgrades[992].s);
-		$('#DJC4Message').html('<b>Worldly Desires</b><br>Your RNG state is: ' + DJC4RNG.state + '.');
-		$('#DJC4Forecast').html('<b>Forecast</b><br><ol></ol>')
-		.append($('<button class="btn btn-link" type="button" />').html('Give me a longer Forecast').on('click', forecastDJC4More));
+		$('#DJC4Message').html('<b>世界之愿</b><br>你的随机数生成器状态: ' + DJC4RNG.state + '.');
+		$('#DJC4Forecast').html('<b>预报</b><br><ol></ol>')
+		.append($('<button class="btn btn-link" type="button" />').html('给我更久点的预报').on('click', forecastDJC4More));
 		forecastDJC4More(save);
 	};
 	//assistants, gem production, max mana
@@ -544,7 +544,7 @@
 		if(DJC4RNG) {
 			for (var i = 0; i < 10; ++i) {
 				var hit = DJC4Hits[DJC4RNG.nextIntRange(0,DJC4Hits.length - 1)];
-				var textResult = 'Increasing Assistants by ' + hit[0] + '%, Production Bonus from Gems by ' + hit[1] + '% and Maximum Mana by ' + hit[2] + '%.';
+				var textResult = '增加助手数量 ' + hit[0] + '%, 宝石产能加成 ' + hit[1] + '% 以及魔上限 ' + hit[2] + '%.';
 				var li = $('<li />').html(textResult);;
 				$('#DJC4Forecast > ol').append(li);
 			}
@@ -582,7 +582,7 @@
 			$('#doSaveCopy').on('click', function(e) {
 				$('#saveInput').trigger('focus');
 				var save = $('#saveInput').val();
-				window.prompt('Copy to clipboard: Press Ctrl+C, then Enter', save);
+				window.prompt('复制到剪贴板: 按Ctrl+C, 然后Enter', save);
 			});
 
 			// Bind Clear button to clear the save input field
